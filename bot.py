e695d7d277fc417f80bf9f2e7307720415f9c3a7
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-09-18 23:58:13 +0300

    2020-09-18 echo-bot

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 import telebot
-import token
+import cofig
 
-bot = telebot.TeleBot(token.TOKEN)
+bot = telebot.TeleBot(cofig.token)
 
-bot.polling()
+
+@bot.message_handler(content_types=['text'])
+def all_messages(message):
+    if message.text != '/start':
+        bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
+
