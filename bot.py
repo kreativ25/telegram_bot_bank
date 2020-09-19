8624735ab11136ba7677dd5004a84961f5ee1acb
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-09-19 22:12:00 +0300

    2020-09-19 markup

--- bot.py
+++ bot.py
@@ -6,10 +6,20 @@ bot = telebot.TeleBot(cofig.token)
 
 @bot.message_handler(content_types=['text'])
 def all_messages(message):
-    if message.text != '/start':
-        bot.send_message(message.chat.id, message.text)
+    # if message.text != '/start':
+    #     bot.send_message(message.chat.id, message.text)
+
+    markup = telebot.types.ReplyKeyboardMarkup()
+    markup.row('Курсы Нацбанка', 'Курсы банков')
+    bot.send_message(message.chat.id, 'Курс не установлен!!!', reply_markup=markup)
+
+
+# @bot.message_handler(content_types=['text'])
+# def curs(message_kb):
+#     markup_kb = telebot.types.ReplyKeyboardMarkup()
+#     markup_kb.row('Курсы банков')
+#     bot.send_message(message_kb.chat.id, 'Курс банков пока не установлен!!!', reply_markup=markup_kb)
 
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
-
