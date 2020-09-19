65bb58c653126f25e13a92a963cdd929bb364cc9
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-09-19 23:36:41 +0300

    2020-09-19 InlineKeyboard

--- bot.py
+++ bot.py
@@ -13,12 +13,10 @@ def all_messages(message):
     markup.row('Курсы Нацбанка', 'Курсы банков')
     bot.send_message(message.chat.id, 'Курс не установлен!!!', reply_markup=markup)
 
-
-# @bot.message_handler(content_types=['text'])
-# def curs(message_kb):
-#     markup_kb = telebot.types.ReplyKeyboardMarkup()
-#     markup_kb.row('Курсы банков')
-#     bot.send_message(message_kb.chat.id, 'Курс банков пока не установлен!!!', reply_markup=markup_kb)
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    url_button = telebot.types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
 
 
 if __name__ == '__main__':
