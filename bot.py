c5f54c63962b657a5dec1e14eda85c574a7a6c1f
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-09-20 20:50:39 +0300

    2020-09-20 message_handler

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import cofig
+from telebot import types
 
 bot = telebot.TeleBot(cofig.token)
 
@@ -9,14 +10,23 @@ def all_messages(message):
     # if message.text != '/start':
     #     bot.send_message(message.chat.id, message.text)
 
-    markup = telebot.types.ReplyKeyboardMarkup()
+    markup = types.ReplyKeyboardMarkup()
     markup.row('Курсы Нацбанка', 'Курсы банков')
-    bot.send_message(message.chat.id, 'Курс не установлен!!!', reply_markup=markup)
+    if message.text == 'Курсы Нацбанка':
+        bot.send_message(message.chat.id, 'Курс Нацбанка не установлен!!!', reply_markup=markup)
+    if message.text == 'Курсы банков':
+        bot.send_message(message.chat.id, 'Курс Банков не установлен!!!', reply_markup=markup)
 
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    url_button = telebot.types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
-    keyboard.add(url_button)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
+
+    #
+    # markup = telebot.types.ReplyKeyboardMarkup()
+    # markup.row('Курсы Нацбанка', 'Курсы банков')
+    # bot.send_message(message.chat.id, 'Курс не установлен!!!', reply_markup=markup)
+    #
+    # keyboard = telebot.types.InlineKeyboardMarkup()
+    # url_button = telebot.types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    # keyboard.add(url_button)
+    # bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
 
 
 if __name__ == '__main__':
