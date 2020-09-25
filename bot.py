0243a0dddb3dc44e565e5e62d66a5a9081985625
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-09-25 11:33:18 +0300

    2020-09-25 curs nbrb

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import telebot
 import cofig
 from telebot import types
+import api_nbrb_curs
 
 bot = telebot.TeleBot(cofig.token)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcom(message):
+    send_text = 'Добро пожаловать! Бот Нацбанка готов к работе :)'
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row('Курсы Нацбанка', 'Курсы банков')
+    if message.text == '/start':
+        bot.send_message(message.chat.id, send_text, reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
@@ -10,13 +20,12 @@ def all_messages(message):
     # if message.text != '/start':
     #     bot.send_message(message.chat.id, message.text)
 
-    markup = types.ReplyKeyboardMarkup()
-    markup.row('Курсы Нацбанка', 'Курсы банков')
-    if message.text == 'Курсы Нацбанка':
-        bot.send_message(message.chat.id, 'Курс Нацбанка не установлен!!!', reply_markup=markup)
-    if message.text == 'Курсы банков':
-        bot.send_message(message.chat.id, 'Курс Банков не установлен!!!', reply_markup=markup)
+    send_usd_text = 'На {} курс usd: {}'.format(api_nbrb_curs.get_usd_data(), api_nbrb_curs.get_usd_curs())
 
+    if message.text == 'Курсы Нацбанка':
+        bot.send_message(message.chat.id, send_usd_text)
+    if message.text == 'Курсы банков':
+        bot.send_message(message.chat.id, 'Курс Банков не установлен!!!')
 
     #
     # markup = telebot.types.ReplyKeyboardMarkup()
