23e17969636999b9bc4cad3fef8f9d05ac6fb51b
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-10-08 21:12:32 +0300

    2020-10-08 Главное меню (НБ и КБ)

--- echo/main.py
+++ echo/main.py
@@ -5,14 +5,56 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
 from telegram.ext import CallbackContext
+from telegram import InlineKeyboardMarkup
+from telegram import InlineKeyboardButton
+from telegram.ext import CallbackQueryHandler
+from telegram import KeyboardButton
+from telegram import ReplyKeyboardMarkup
 
 from echo.cofig import token
+
+CALLBACK_BUTTON_1_MENU_NB = 'nb_menu'
+CALLBACK_BUTTON_2_MENU_KB = 'kb_menu'
+CALLBACK_BUTTON_STAVKI_NB = 'stavki_nb'
+CALLBACK_BUTTON_CURS_NB = 'curs_nb'
+CALLBACK_BUTTON_LIQ_NB = 'liq_nb'
+CALLBACK_BUTTON_MBK_NB = 'mbk_nb'
+CALLBACK_BUTTON_NEWS_NB = 'news_nb'
+CALLBACK_BUTTON_METALL_NB = 'metall_nb'
+CALLBACK_BUTTON_BACK_NB = 'back_nb'
+
+TITLES = {
+    CALLBACK_BUTTON_1_MENU_NB: 'Национальный банк',
+    CALLBACK_BUTTON_2_MENU_KB: 'Банки',
+    CALLBACK_BUTTON_STAVKI_NB: 'Ставки НБ',
+    CALLBACK_BUTTON_CURS_NB: 'Курсы валют НБ',
+    CALLBACK_BUTTON_LIQ_NB: 'Ликвидность',
+    CALLBACK_BUTTON_MBK_NB: 'МБК',
+    CALLBACK_BUTTON_NEWS_NB: 'Новости',
+    CALLBACK_BUTTON_METALL_NB: 'Драг. металлы',
+    CALLBACK_BUTTON_BACK_NB: '⬅️ Назад'
+}
+
+
+# главное меню
+def get_base_menu():
+    keyboard = [
+        [
+            KeyboardButton(TITLES[CALLBACK_BUTTON_1_MENU_NB]),
+            KeyboardButton(TITLES[CALLBACK_BUTTON_2_MENU_KB])
+        ],
+    ]
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
 
 
 # функция обрабатывает комманду start
 def do_start(update: Update, context: CallbackContext):
     update.message.reply_text(
-        text='Добро пожаловать! :)'
+        text='Добро пожаловать! :)',
+        reply_markup=get_base_menu(),
     )
 
 
