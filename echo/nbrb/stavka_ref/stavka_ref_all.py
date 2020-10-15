efdca360152e91f78af61a28b49be28de04f3690
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-10-15 11:57:19 +0300

    2020-10-15 готов график динамики ставки реф

--- echo/nbrb/stavka_ref/stavka_ref_all.py
+++ echo/nbrb/stavka_ref/stavka_ref_all.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import io
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image
 import requests as rq
 import datetime as dt
-import pathlib
 from dateutil.parser import *
 
 sr = rq.get('https://www.nbrb.by/api/refinancingrate', verify=False)
@@ -17,22 +16,27 @@ for i in sr_json:
     date.append(dt.datetime.date(date_prepare).strftime('%Y.%m.%d'))
     value.append(i['Value'])
 
-date_len = 10
+date_len = 15
 date = date[-date_len:]
 value = value[-date_len:]
 
-
-plt.figure(figsize=(16, 10), dpi=80)
+plt.figure(figsize=(24, 12), dpi=80)
 plt.plot(date, value, color='tab:red')
 
-plt.scatter(x=date, y=value, color='tab:red',  s=15)
+# размер точек на линии
+plt.scatter(x=date, y=value, color='tab:red', s=30)
 
-plt.yticks(fontsize=25, alpha=.5)
-plt.xticks(fontsize=10, alpha=.5)
-plt.title('Динамика ставки рефинансирования', fontsize=45, pad=35, alpha=.5)
+# подпись оси у
+plt.yticks(fontsize=20, alpha=.5)
 
+# подпись оси х / rotation=90 - переварачивваем даты
+plt.xticks(fontsize=14, alpha=.5, rotation=90)
 
-plt.grid(axis='both', alpha=.3)
+# название графика
+plt.title('Динамика ставки рефинансирования', fontsize=45, pad=45, alpha=.5)
+
+# подписи точек
+plt.grid(axis='both', alpha=.5)
 for i in range(date_len):
     plt.text(date[i], value[i], value[i], horizontalalignment='left', fontsize=35, alpha=1)
 
@@ -43,8 +47,13 @@ plt.gca().spines["right"].set_alpha(0.0)
 plt.gca().spines["left"].set_alpha(0.3)
 # plt.show()
 
+
 buf = io.BytesIO()
 plt.savefig(buf, format='png', dpi=80)
 buf.seek(0)
 im = Image.open(buf)
 im.show()
+
+
+def get_plot_sr_all():
+    return im
