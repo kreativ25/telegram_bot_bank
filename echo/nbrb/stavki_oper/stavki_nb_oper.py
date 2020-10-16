591aba42cadc74b40fa095b360115b1a9760c5ab
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-10-16 21:19:39 +0300

    2020-10-16 готовы ставки по операциям НБ с датами

--- echo/nbrb/stavki_oper/stavki_nb_oper.py
+++ echo/nbrb/stavki_oper/stavki_nb_oper.py
@@ -1,11 +1,13 @@
 import requests as rq
 import xlrd
+import datetime as dt
 
 # подключаем экселевский файл
 url_nb_oper = 'http://www.nbrb.by/statistics/monetarypolicyinstruments/interestrates/intratesnbrb_archive.xlsx'
 r = rq.get(url_nb_oper)
 
 wb = xlrd.open_workbook(file_contents=r.content)
+wb_date = xlrd.open_workbook(file_contents=r.content)
 wb_range = max(range(wb.nsheets))  # номер максимальной страницы
 wb = wb.sheet_by_index(wb_range)
 
@@ -36,16 +38,16 @@ for row in range(wb_nrows):
                 if type(wb.cell(row, data).value) == float:
                     dabl_kredit.append(wb.cell(row, data).value * 100)
 
+        if wb.cell(row, col).value == 'Операции':
+            for data in range(wb_ncols):
+                if type(wb.cell(row, data).value) == float:
+                    date_prepare = xlrd.xldate_as_tuple(wb.cell(row, data).value, wb_date.datemode)
+                    date_prepare = dt.datetime(date_prepare[0], date_prepare[1], date_prepare[2])
+                    date_prepare = dt.datetime.date(date_prepare).strftime('%Y.%m.%d')
+                    data_stavki.append(date_prepare)
+
 
 print(kredit_over_stavki)
 print(depozit_over_stavki)
 print(dabl_kredit)
-
-
-
-# дата
-# кредит овернайт
-# ставка реф
-# деп овернайт
-# двусторонний кредит
-# тс
+print(data_stavki)
