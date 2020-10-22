e3885a8e785947ed4a42ab41e42498ca1ce5f1c8
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-10-22 13:05:14 +0300

    2020-10-22 готов генератор png по текущим ставкам НБ на фин рынке

---
+++ echo/nbrb/stavki_oper/stavki_oper_nb_one.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageFont, ImageDraw
+import pathlib
+from echo.nbrb.stavki_oper.stavki_nb_oper import *
+
+
+data = dt.datetime.date(dt.datetime.now()).__str__()
+
+img_path = pathlib.Path('stavki_oper_one.png')
+font_path = pathlib.Path('OpenSans-Regular.ttf').__str__()
+
+img = Image.open(img_path)
+data_font = ImageFont.truetype(font_path, 45)
+stavka_font = ImageFont.truetype(font_path, 80)
+
+img_draw = ImageDraw.Draw(img)
+img_draw.text((520, 83), data, font=data_font, fill=(153, 0, 51))
+img_draw.text((550, 233), get_kredit_over_stavki_one(), font=stavka_font, fill=(134, 31, 45))
+img_draw.text((550, 360), get_dabl_kredit_stavki_one(), font=stavka_font, fill=(134, 31, 45))
+img_draw.text((550, 510), get_depozit_over_stavki_one(), font=stavka_font, fill=(134, 31, 45))
+img.show()
+
+
+def get_stavki_oper_one():
+    return img
+
