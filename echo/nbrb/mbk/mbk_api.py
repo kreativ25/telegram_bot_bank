6dc9263689bf19f761d3124d1c3060230cb9dab5
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-11-12 19:09:19 +0300

    2020-10-12 запросы

--- echo/nbrb/mbk/mbk_api.py
+++ echo/nbrb/mbk/mbk_api.py
@@ -7,22 +7,18 @@ import pymysql as pm
 import echo.config as cf
 from bs4 import BeautifulSoup
 
+date_satrt = dt.date(2019, 10, 1)
 
-date_satrt = dt.date(2019, 10, 15)
-# date_satrt = date_satrt + + dt.timedelta(days=1)
+date = dt.datetime.now().date()
+date_yesterday = dt.datetime.now().date() - dt.timedelta(days=1)
 
+print(date)
 
 # Блок подключения к БД MySQL
 connection = pm.connect(host=cf.host,
                         user=cf.user,
                         password=cf.password,
                         db=cf.db)
-
-# запрос к MySQL - максимальная дата
-# mbk = connection.cursor()
-# mbk.execute("select max(date) from mbk")
-# max_date_mbk_bd = mbk.fetchone()
-# connection.commit()
 
 url = 'https://www.nbrb.by/statistics/financialmarkets/interbankrates'
 
@@ -79,8 +75,8 @@ data_mbk = {
 }
 
 # запись спарсеных данных в MySQL
-# mbk = connection.cursor()
-# mbk.execute(
+# mbk_pars = connection.cursor()
+# mbk_pars.execute(
 #     "INSERT INTO mbk (date, mbk_sum, mbk_stavka, time_stamp)"
 #     " VALUES (%s, %s, %s, %s)",
 #     (data_mbk['date'], data_mbk['mbk_sum'], data_mbk['mbk_stavka'], time_stamp))
@@ -90,8 +86,8 @@ data_mbk = {
 
 
 # обновление последней даты записи time_stamp
-# mbk = connection.cursor()
-# mbk.execute(
+# mbk_up_date = connection.cursor()
+# mbk_up_date.execute(
 #     "UPDATE mbk SET time_stamp = %s  order by date(date) desc limit 1",
 #     time_stamp)
 # connection.commit()
@@ -113,3 +109,22 @@ data_mbk = {
 # diff = time_stamp - mbk_max_ts[0]
 # delta_hours = int(diff.seconds // (60*60))
 # delta_mins = int((diff.seconds // 60) % 60)
+
+
+# запрос к MySQL - максимальная дата
+# mbk_max_date = connection.cursor()
+# mbk_max_date.execute("select max(date) from mbk")
+# max_date_mbk_bd = mbk_max_date.fetchone()
+# connection.commit()
+#
+# print(max_date_mbk_bd[0])
+
+
+
+
+
+# ==========================================================================
+# блок пополнения БД
+date_satrt = dt.date(2019, 10, 15)
+date_satrt = date_satrt + dt.timedelta(days=1)
+
