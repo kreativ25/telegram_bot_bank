43c1121a4255a27e5cc2c9732be63b243713b0ff
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-11-28 22:17:43 +0300

    2020-11-28 скрипт пополнения бд - ставка рефинансирования - все значения

--- echo/scripts/sr_all.py
+++ echo/scripts/sr_all.py
@@ -4,12 +4,57 @@ import requests as rq
 import pymysql as pm
 import echo.config as cf
 
-# делаем стабильное подключение с реконектом = 7 раз
+# получаем текущую ставку реф
+data = dt.datetime.date(dt.datetime.now()).__str__()
+params = {'ondate': data}
+
+# получаем максимальную дату api
 url = 'https://www.nbrb.by/api/refinancingrate'
-adapter = HTTPAdapter(max_retries=7)
+adapter = HTTPAdapter(max_retries=10)
 with rq.Session() as session:
     session.mount(url, adapter)
-    response = session.get(url)
+    sr_new = session.get(url, params=params)
+date_api = dt.datetime.strptime(sr_new.json()[0]['Date'][:-9].replace('-', '.'), '%Y.%m.%d').date()
 
-sr_all_result = response.json()
-print(sr_all_result)
+# получаем максимальную дату БД
+connection = pm.connect(host=cf.host,
+                        user=cf.user,
+                        password=cf.password,
+                        db=cf.db)
+
+cur = connection.cursor()
+cur.execute("select max(date) from sr_all")
+date_bd = cur.fetchone()
+connection.commit()
+
+
+if date_api != date_bd[0] or date_bd is None:
+
+    # формируем полный список данных
+    url = 'https://www.nbrb.by/api/refinancingrate'
+    adapter = HTTPAdapter(max_retries=10)
+    with rq.Session() as session:
+        session.mount(url, adapter)
+        response = session.get(url)
+    sr_all = response.json()
+
+    sr_all_result = {}
+    for i in range(len(sr_all)):
+        sr_all_result[i] = {
+            'date': sr_all[i]['Date'],
+            'value': sr_all[i]['Value']
+        }
+
+    # очищаем таблицу
+    sr = connection.cursor()
+    sr.execute('truncate table sr_all')
+    connection.commit()
+
+    for i in range(len(sr_all_result)):
+        time_stamp = dt.datetime.now()
+        sr = connection.cursor()
+        sr.execute(
+            "INSERT HIGH_PRIORITY INTO sr_all (date, sr, time_stamp)"
+            " VALUES (%s, %s, %s)",
+            (sr_all_result[i]['date'], sr_all_result[i]['value'], time_stamp))
+        connection.commit()
