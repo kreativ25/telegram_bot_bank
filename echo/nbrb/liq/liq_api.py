bbe1e0d72b0673e90304700ca3905700e0ff39e2
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-11-30 16:24:08 +0300

    2020-11-30 почистил скрипт получения данных для get функций (ликвидность)

--- echo/nbrb/liq/liq_api.py
+++ echo/nbrb/liq/liq_api.py
@@ -1,7 +1,3 @@
-from requests import Session
-from requests.adapters import HTTPAdapter
-import xlrd
-import re
 import datetime as dt
 import pymysql as pm
 import echo.config as cf
@@ -11,112 +7,6 @@ connection = pm.connect(host=cf.host,
                         user=cf.user,
                         password=cf.password,
                         db=cf.db)
-
-
-# запрос к MySQL - максимальная дата
-cur = connection.cursor()
-cur.execute("select max(date) from liq")
-max_date_mysql = cur.fetchone()
-connection.commit()
-# print('1) ', max_date_mysql, '<-- максимальная дата Mysql')
-
-
-# проверяем время последнего обновления информации
-cur = connection.cursor()
-cur.execute('select max(time_stamp) as max_ts from liq')
-max_ts_mysql = cur.fetchone()
-# print('2) ', max_ts_mysql, '<-- max time_stamp Mysql')
-
-
-# получаем список дат в БД MySQL
-cur = connection.cursor()
-cur.execute('select date as date_mysql from liq')
-date_list_mysql = cur.fetchall()
-# print('3)  список дат mysql -->', date_list_mysql)
-
-list_date_mysql = []
-for i in date_list_mysql:
-    list_date_prepare = dt.datetime.strftime(i[0], '%Y.%m.%d')
-    list_date_mysql.append(list_date_prepare)
-# print('4)  список дат mysql  -->', list_date_mysql)
-
-
-# обновление базы данных MySQL при наличии новых данных на сайте
-time_stamp = dt.datetime.now()
-
-if (dt.datetime.now().minute - dt.datetime.time(max_ts_mysql[0]).minute) > 10:
-# if 1<2:
-    url = 'https://www.nbrb.by/mp/liquidity/arch/liqudity_ru.xlsx'
-
-    adapter = HTTPAdapter(max_retries=7)
-    with Session() as session:
-        session.mount(url, adapter)
-        response = session.get(url)
-
-    wb = xlrd.open_workbook(file_contents=response.content)
-    wb_date = xlrd.open_workbook(file_contents=response.content)
-    wb_range = max(range(wb.nsheets))  # номер максимальной страницы
-    wb = wb.sheet_by_index(wb_range)
-
-    wb_ncols = wb.ncols  # количество столбцов
-    wb_nrows = wb.nrows  # количество строк
-
-    date_liq = []
-    liq = []
-    prt = []
-    psi = []
-
-    for row in range(wb_nrows):
-        for col in range(wb_ncols):
-
-            # вытягиваем и форматируем массив дат
-            if re.search(r'\bОстаток средств на коррсчетах\b', str(wb.cell(row, col).value)):
-                for data in range(wb_ncols):
-                    if type(wb.cell(row, data).value) == float:
-                        seconds = (wb.cell(row-2, data).value - 25569) * 86400.0
-                        date_prepare = dt.datetime.utcfromtimestamp(seconds)
-                        date_prepare = dt.datetime.date(date_prepare).strftime('%Y.%m.%d')
-                        date_liq.append(date_prepare)
-
-            # Ликвидность
-            if re.search(r'\bПоказатели ликвидности\b', str(wb.cell(row, col).value)):
-                for data in range(wb_ncols):
-                    if type(wb.cell(row, data).value) == float:
-                        liq.append(wb.cell(row, data).value)
-
-            # ПРТ
-            if re.search(r'\bПозиция по резервным требованиям\b', str(wb.cell(row, col).value)):
-                for data in range(wb_ncols):
-                    if type(wb.cell(row, data).value) == float:
-                        prt.append(wb.cell(row, data).value)
-
-            # ПСИ
-            if re.search(r'\bПозиция по стандартным инструментам\b', str(wb.cell(row, col).value)):
-                for data in range(wb_ncols):
-                    if type(wb.cell(row, data).value) == float:
-                        psi.append(wb.cell(row, data).value)
-
-    max_date = dt.datetime.strptime(max(date_liq), '%Y.%m.%d').date()
-
-    # проверяем есть ли новые ставки
-    if max_date_mysql[0] < max_date:
-    # if 1 < 2:
-
-        print('Есть новые данные')
-
-        x = -1
-        for i in date_liq:
-            if i in list_date_mysql:
-                x = x+1
-                print(x, i, ' присутствует')
-            else:
-                x = x + 1
-                print(x, i, ' NO')
-                cur = connection.cursor()
-                cur.execute("INSERT LOW_PRIORITY INTO liq (date, liq, prt, psi, time_stamp) VALUES (%s, %s, %s, %s, %s)",
-                            (date_liq[x], liq[x], prt[x], psi[x], time_stamp))
-                connection.commit()
-
 
 # получаем данные из MySQL для get функций
 cur = connection.cursor()
@@ -159,16 +49,3 @@ def get_liq_delta():
 
     }
     return liq_delta
-
-
-
-
-# cur = connection.cursor()
-# cur.execute("INSERT INTO liq (date, liq, prt, psi, time_stamp) VALUES (%s, %s, %s, %s, %s)",
-#             (date_liq[0], liq[0], prt[0], psi[0], time_stamp))
-# connection.commit()
-#
-# cur = connection.cursor()
-# cur.execute("INSERT INTO liq (date, liq, prt, psi, time_stamp) VALUES (%s, %s, %s, %s, %s)",
-#             (date_liq[1], liq[1], prt[1], psi[1], time_stamp))
-# connection.commit()
