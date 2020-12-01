7bc895ccbbbc5bcc96f75aae6996b45c9b1af270
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-12-01 15:40:12 +0300

    2020-12-01 доработал создание картинки - текущие курсы валют НБ

--- echo/nbrb/kurs/kurs_nb_one.py
+++ echo/nbrb/kurs/kurs_nb_one.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageFont, ImageDraw
 import pathlib
-from echo.nbrb.kurs.api_nbrb_curs import *
+# from echo.nbrb.kurs.api_nbrb_curs import *
+
+from echo.nbrb.kurs.get_func_curs import *
 
 img = Image.new("RGB", (1200, 800), (255, 255, 255))
 img_draw = ImageDraw.Draw(img)
@@ -25,32 +27,31 @@ line = '_______________________________________________'
 img_draw.text((60, 20), name, font=name_font, fill=(134, 31, 45))
 
 img_draw.text((350, 100), date, font=date_font, fill=(134, 31, 45))
-img_draw.text((570, 100), get_date_kurs_nb(), font=date_font, fill=(134, 31, 45))
+img_draw.text((570, 100), str(max_date_curs_nb()), font=date_font, fill=(134, 31, 45))
 
 img_draw.text((360, 230), usd, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 230), str(get_kurs_nb()['usd']), font=kurs_font, fill=(134, 31, 45))
+img_draw.text((620, 230), str(curs_nb_one(max_date_curs_nb())['usd']), font=kurs_font, fill=(134, 31, 45))
 img_draw.text((360, 290), line, font=line_font, fill=(20, 11, 15))
 
 img_draw.text((360, 330), eur, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 330), str(get_kurs_nb()['eur']), font=kurs_font, fill=(134, 31, 45))
+img_draw.text((620, 330), str(curs_nb_one(max_date_curs_nb())['eur']), font=kurs_font, fill=(134, 31, 45))
 img_draw.text((360, 390), line, font=line_font, fill=(20, 11, 15))
 
 img_draw.text((360, 430), rub, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 430), str(get_kurs_nb()['rub']), font=kurs_font, fill=(134, 31, 45))
+img_draw.text((620, 430), str(curs_nb_one(max_date_curs_nb())['rub']), font=kurs_font, fill=(134, 31, 45))
 img_draw.text((360, 490), line, font=line_font, fill=(20, 11, 15))
 
 
 img_draw.text((360, 530), uah, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 530), str(get_kurs_nb()['uah']), font=kurs_font, fill=(134, 31, 45))
+img_draw.text((620, 530), str(curs_nb_one(max_date_curs_nb())['uah']), font=kurs_font, fill=(134, 31, 45))
 img_draw.text((360, 590), line, font=line_font, fill=(20, 11, 15))
 
 
 img_draw.text((360, 630), pln, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 630), str(get_kurs_nb()['pln']), font=kurs_font, fill=(134, 31, 45))
+img_draw.text((620, 630), str(curs_nb_one(max_date_curs_nb())['pln']), font=kurs_font, fill=(134, 31, 45))
 img_draw.text((360, 690), line, font=line_font, fill=(20, 11, 15))
 
 # img.show()
-
 
 def get_kurs_nb_one():
     return img
