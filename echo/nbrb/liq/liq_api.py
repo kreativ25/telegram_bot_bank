929c2cba93bf530d55e95aee8860e947c8bdff37
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-12-09 16:19:38 +0300

    2020-12-09 доработал ликвидность

--- echo/nbrb/liq/liq_api.py
+++ echo/nbrb/liq/liq_api.py
@@ -8,40 +8,6 @@ connection = pm.connect(host=cf.host,
                         password=cf.password,
                         db=cf.db)
 
-date_liq_mysql = []
-liq_mysql = []
-prt_mysql = []
-psi_mysql = []
-
-
-# выбираем все даты из MySQL
-def get_date_liq_all():
-    date = (dt.datetime.now().date() - dt.timedelta(days=360))
-    cur = connection.cursor()
-    cur.execute(f"select date as _date from liq where date > '{date}' ORDER BY date")
-    data_mysql = cur.fetchall()
-    connection.commit()
-
-    for i in data_mysql:
-        date_liq_mysql_prepare = dt.datetime.strptime(dt.datetime.strftime(i[0], '%Y.%m.%d'), '%Y.%m.%d').date()
-        date_liq_mysql.append(date_liq_mysql_prepare)
-
-    return date_liq_mysql
-
-
-# список значений ликвидности
-def get_liq_all():
-    date = (dt.datetime.now().date() - dt.timedelta(days=360))
-    cur = connection.cursor()
-    cur.execute(f"select liq as _liq from liq where date > '{date}' ORDER BY date")
-    data_mysql = cur.fetchall()
-    connection.commit()
-
-    for i in data_mysql:
-        liq_mysql.append(i[0])
-
-    return liq_mysql
-
 
 # возвращаем данные на последнюю дату в MySQL
 def get_liq_data_old():
--- echo/nbrb/liq/liq_dinamic.py
+++ echo/nbrb/liq/liq_dinamic.py
@@ -1,61 +1,130 @@
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import io
 from PIL import Image
-from echo.nbrb.liq.liq_api import get_liq_all, get_date_liq_all
-
-# устанавливаем количество отображаемых дат
-date_len = 360
-
-# ось х
-x = get_date_liq_all()
-x = x[:date_len]
-
-# ось y - приводим к float формату
-y = get_liq_all()
-y_prepare = []
-for i in y:
-    y_prepare.append(float(i))
-y = np.array(y_prepare)
-y = y[:date_len]
-
-plt.figure(figsize=(18, 12), dpi=100)
-plt.plot(x, y, color='tab:red')
-plt.grid(axis='both', alpha=.5)
-
-# размер точек на линии
-plt.scatter(x=x, y=y, color='tab:red', s=10)
-
-# подпись оси у
-plt.yticks(fontsize=30)
-
-# подпись оси х - делаем разрядность подписей - автоформат
-plt.xticks(fontsize=14, alpha=.5, rotation=90)
-
-# название графика
-plt.title('Динамика ликвидности банковской системы, млн. руб. Период - ' + str(date_len) + ' дней.', fontsize=30, pad=45, alpha=1)
-
-# Remove borders
-plt.gca().spines["top"].set_alpha(0.0)
-plt.gca().spines["bottom"].set_alpha(0.9)
-plt.gca().spines["right"].set_alpha(0.0)
-plt.gca().spines["left"].set_alpha(0.3)
-
-# ось х по центру графика
-plt.gca().spines['bottom'].set_position(('data', 0))
-plt.gca().spines['bottom'].set_linewidth(3)
-plt.gca().spines['bottom'].set_color('green')
-
-plt.rcParams.update({'font.size': 28})
-
-# преобразуем в png
-buf = io.BytesIO()
-plt.savefig(buf, format='png', dpi=100)
-buf.seek(0)
-im = Image.open(buf)
-# im.show()
+import datetime as dt
+import pymysql as pm
+import echo.config as cf
+import warnings
 
 
 def get_liq_all():
+    connection = pm.connect(host=cf.host,
+                            user=cf.user,
+                            password=cf.password,
+                            db=cf.db)
+
+    date = (dt.datetime.now().date() - dt.timedelta(days=360))
+    cur = connection.cursor()
+    cur.execute(f"select date, liq from liq where date > '{date}' ORDER BY date")
+    data_mysql = cur.fetchall()
+    connection.commit()
+
+    df = pd.DataFrame(data_mysql)
+
+    df[0] = pd.to_datetime(df[0])
+    df[1] = pd.to_numeric(df[1])
+
+    warnings.simplefilter("ignore", UserWarning)
+
+    plt.figure(figsize=(18, 12), dpi=100)
+    plt.plot(df[0], df[1], color='tab:red')
+    plt.grid(axis='both', alpha=.5)
+
+    # размер точек на линии
+    plt.scatter(x=df[0], y=df[1], color='tab:red', s=10)
+
+    # подпись оси у
+    plt.yticks(fontsize=30)
+
+    # подпись оси х - делаем разрядность подписей - автоформат
+    plt.xticks(fontsize=14, alpha=.5, rotation=90)
+
+    # название графика
+    plt.title('Динамика ликвидности банковской системы, млн. руб. Период - 1 год.', fontsize=30, pad=45, alpha=1)
+
+    # Remove borders
+    plt.gca().spines["top"].set_alpha(0.0)
+    plt.gca().spines["bottom"].set_alpha(0.9)
+    plt.gca().spines["right"].set_alpha(0.0)
+    plt.gca().spines["left"].set_alpha(0.3)
+
+    # ось х по центру графика
+    plt.gca().spines['bottom'].set_position(('data', 0))
+    plt.gca().spines['bottom'].set_linewidth(3)
+    plt.gca().spines['bottom'].set_color('green')
+
+    plt.rcParams.update({'font.size': 28})
+
+    # преобразуем в png
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', dpi=100)
+    buf.seek(0)
+    im = Image.open(buf)
+    # im.show()
+
+    # закрываем все открытые окна графика
+    plt.cla()
+    plt.clf()
+    plt.close('all')
 
     return im
+
+
+
+
+
+#
+# def get_liq_all():
+#     # Блок подключения к БД MySQL
+#     connection = pm.connect(host=cf.host,
+#                             user=cf.user,
+#                             password=cf.password,
+#                             db=cf.db)
+#
+#     date = (dt.datetime.now().date() - dt.timedelta(days=360))
+#     cur = connection.cursor()
+#     cur.execute(f"select date, liq from liq where date > '{date}' ORDER BY date")
+#     data_mysql = cur.fetchall()
+#     connection.commit()
+#
+#     x = [data_mysql[x][0] for x in range(len(data_mysql))]
+#     y = [float(data_mysql[x][1]) for x in range(len(data_mysql))]
+#
+#     plt.figure(figsize=(18, 12), dpi=100)
+#     plt.plot(x, y, color='tab:red')
+#     plt.grid(axis='both', alpha=.5)
+#     #
+#     # размер точек на линии
+#     plt.scatter(x=x, y=y, color='tab:red', s=10)
+#
+#     # подпись оси у
+#     plt.yticks(fontsize=30)
+#
+#     # подпись оси х - делаем разрядность подписей - автоформат
+#     plt.xticks(fontsize=14, alpha=.5, rotation=90)
+#
+#     # название графика
+#     plt.title('Динамика ликвидности банковской системы, млн. руб. Период - 1 год.', fontsize=30, pad=45, alpha=1)
+#
+#     # Remove borders
+#     plt.gca().spines["top"].set_alpha(0.0)
+#     plt.gca().spines["bottom"].set_alpha(0.9)
+#     plt.gca().spines["right"].set_alpha(0.0)
+#     plt.gca().spines["left"].set_alpha(0.3)
+#
+#     # ось х по центру графика
+#     plt.gca().spines['bottom'].set_position(('data', 0))
+#     plt.gca().spines['bottom'].set_linewidth(3)
+#     plt.gca().spines['bottom'].set_color('green')
+#
+#     plt.rcParams.update({'font.size': 28})
+#
+#     # преобразуем в png
+#     buf = io.BytesIO()
+#     plt.savefig(buf, format='png', dpi=100)
+#     buf.seek(0)
+#     im = Image.open(buf)
+#     # im.show()
+#
+#     return im
