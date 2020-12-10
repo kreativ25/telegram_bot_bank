07c8dc2e57403dcd2c2ca6e8a17176a0854b20ef
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2020-12-10 11:17:20 +0300

    2020-12-10 доработал графики курсов валют НБ

--- echo/nbrb/kurs/kurs_nb_eur_all.py
+++ echo/nbrb/kurs/kurs_nb_eur_all.py
@@ -2,48 +2,56 @@ import matplotlib.pyplot as plt
 import io
 from PIL import Image
 from echo.nbrb.kurs.get_func_curs import *
-
-term = 700
-data_eur = curs_nb_all('eur', max_date_curs_nb(), term)
-
-x = []
-y = []
-
-for i in range(len(data_eur)):
-    x.append(data_eur[i][0])
-    y.append(float(data_eur[i][1]))
-
-cur_name = 'Евро'
-
-plt.figure(figsize=(18, 12), dpi=100)
-plt.plot(x, y, color='tab:red', label=cur_name)
-plt.grid(axis='both', alpha=.5)
-
-# размер точек на линии
-plt.scatter(x=x, y=y, color='tab:red', s=10)
-
-# подпись оси у
-plt.yticks(fontsize=15)
-
-# подпись оси х - делаем разрядность подписей - автоформат
-plt.xticks(fontsize=14, alpha=.5, rotation=90)
-
-# название графика
-plt.title('Динамика ' + cur_name + ' - за последние ' + str(term) + ' дней.', fontsize=35, pad=45, alpha=1)
-
-# Remove borders
-plt.gca().spines["top"].set_alpha(0.0)
-plt.gca().spines["bottom"].set_alpha(0.3)
-plt.gca().spines["right"].set_alpha(0.0)
-plt.gca().spines["left"].set_alpha(0.3)
-
-# преобразуем в png
-buf = io.BytesIO()
-plt.savefig(buf, format='png', dpi=100)
-buf.seek(0)
-im = Image.open(buf)
-# im.show()
+import warnings
 
 
 def get_kurs_nb_eur_all():
+
+    term = 700
+    data_eur = curs_nb_all('eur', max_date_curs_nb(), term)
+
+    x = []
+    y = []
+
+    for i in range(len(data_eur)):
+        x.append(data_eur[i][0])
+        y.append(float(data_eur[i][1]))
+
+    cur_name = 'Евро'
+
+    warnings.simplefilter("ignore", UserWarning)
+    plt.figure(figsize=(18, 12), dpi=100)
+    plt.plot(x, y, color='tab:red', label=cur_name)
+    plt.grid(axis='both', alpha=.5)
+
+    # размер точек на линии
+    plt.scatter(x=x, y=y, color='tab:red', s=10)
+
+    # подпись оси у
+    plt.yticks(fontsize=15)
+
+    # подпись оси х - делаем разрядность подписей - автоформат
+    plt.xticks(fontsize=14, alpha=.5, rotation=90)
+
+    # название графика
+    plt.title('Динамика ' + cur_name + ' - за последние ' + str(term) + ' дней.', fontsize=35, pad=45, alpha=1)
+
+    # Remove borders
+    plt.gca().spines["top"].set_alpha(0.0)
+    plt.gca().spines["bottom"].set_alpha(0.3)
+    plt.gca().spines["right"].set_alpha(0.0)
+    plt.gca().spines["left"].set_alpha(0.3)
+
+    # преобразуем в png
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', dpi=100)
+    buf.seek(0)
+    im = Image.open(buf)
+    # im.show()
+
+    # закрываем все открытые окна графика
+    plt.cla()
+    plt.clf()
+    plt.close('all')
+
     return im
--- echo/nbrb/kurs/kurs_nb_one.py
+++ echo/nbrb/kurs/kurs_nb_one.py
@@ -3,55 +3,53 @@ import pathlib
 from echo.nbrb.kurs.get_func_curs import *
 
 
-img = Image.new("RGB", (1200, 800), (255, 255, 255))
-img_draw = ImageDraw.Draw(img)
-
-font_path = pathlib.Path('font/Open_Sans/OpenSans-Regular.ttf').__str__()
-# font_path = pathlib.Path('OpenSans-Regular.ttf').__str__()
-
-name_font = ImageFont.truetype(font_path, 60)
-date_font = ImageFont.truetype(font_path, 50)
-kurs_font = ImageFont.truetype(font_path, 50)
-line_font = ImageFont.truetype(font_path, 20)
-
-name = 'Курсы валют Национального банка'
-date = 'на дату:'
-usd = '1 USD:'
-eur = '1 EUR:'
-rub = '100 RUB:'
-uah = '100 UAH:'
-pln = '10 PLN:'
-line = '_______________________________________________'
-
-img_draw.text((60, 20), name, font=name_font, fill=(134, 31, 45))
-
-img_draw.text((350, 100), date, font=date_font, fill=(134, 31, 45))
-img_draw.text((570, 100), str(max_date_curs_nb()), font=date_font, fill=(134, 31, 45))
-
-img_draw.text((360, 230), usd, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 230), str(curs_nb_one(max_date_curs_nb())['usd']), font=kurs_font, fill=(134, 31, 45))
-img_draw.text((360, 290), line, font=line_font, fill=(20, 11, 15))
-
-img_draw.text((360, 330), eur, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 330), str(curs_nb_one(max_date_curs_nb())['eur']), font=kurs_font, fill=(134, 31, 45))
-img_draw.text((360, 390), line, font=line_font, fill=(20, 11, 15))
-
-img_draw.text((360, 430), rub, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 430), str(curs_nb_one(max_date_curs_nb())['rub']), font=kurs_font, fill=(134, 31, 45))
-img_draw.text((360, 490), line, font=line_font, fill=(20, 11, 15))
-
-
-img_draw.text((360, 530), uah, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 530), str(curs_nb_one(max_date_curs_nb())['uah']), font=kurs_font, fill=(134, 31, 45))
-img_draw.text((360, 590), line, font=line_font, fill=(20, 11, 15))
-
-
-img_draw.text((360, 630), pln, font=date_font, fill=(134, 31, 45))
-img_draw.text((620, 630), str(curs_nb_one(max_date_curs_nb())['pln']), font=kurs_font, fill=(134, 31, 45))
-img_draw.text((360, 690), line, font=line_font, fill=(20, 11, 15))
-
-# img.show()
-
-
 def get_kurs_nb_one():
+
+    img = Image.new("RGB", (1200, 800), (255, 255, 255))
+    img_draw = ImageDraw.Draw(img)
+
+    font_path = pathlib.Path('font/Open_Sans/OpenSans-Regular.ttf').__str__()
+    # font_path = pathlib.Path('OpenSans-Regular.ttf').__str__()
+
+    name_font = ImageFont.truetype(font_path, 60)
+    date_font = ImageFont.truetype(font_path, 50)
+    kurs_font = ImageFont.truetype(font_path, 50)
+    line_font = ImageFont.truetype(font_path, 20)
+
+    name = 'Курсы валют Национального банка'
+    date = 'на дату:'
+    usd = '1 USD:'
+    eur = '1 EUR:'
+    rub = '100 RUB:'
+    uah = '100 UAH:'
+    pln = '10 PLN:'
+    line = '_______________________________________________'
+
+    img_draw.text((60, 20), name, font=name_font, fill=(134, 31, 45))
+
+    img_draw.text((350, 100), date, font=date_font, fill=(134, 31, 45))
+    img_draw.text((570, 100), str(max_date_curs_nb()), font=date_font, fill=(134, 31, 45))
+
+    img_draw.text((360, 230), usd, font=date_font, fill=(134, 31, 45))
+    img_draw.text((620, 230), str(curs_nb_one(max_date_curs_nb())['usd']), font=kurs_font, fill=(134, 31, 45))
+    img_draw.text((360, 290), line, font=line_font, fill=(20, 11, 15))
+
+    img_draw.text((360, 330), eur, font=date_font, fill=(134, 31, 45))
+    img_draw.text((620, 330), str(curs_nb_one(max_date_curs_nb())['eur']), font=kurs_font, fill=(134, 31, 45))
+    img_draw.text((360, 390), line, font=line_font, fill=(20, 11, 15))
+
+    img_draw.text((360, 430), rub, font=date_font, fill=(134, 31, 45))
+    img_draw.text((620, 430), str(curs_nb_one(max_date_curs_nb())['rub']), font=kurs_font, fill=(134, 31, 45))
+    img_draw.text((360, 490), line, font=line_font, fill=(20, 11, 15))
+
+    img_draw.text((360, 530), uah, font=date_font, fill=(134, 31, 45))
+    img_draw.text((620, 530), str(curs_nb_one(max_date_curs_nb())['uah']), font=kurs_font, fill=(134, 31, 45))
+    img_draw.text((360, 590), line, font=line_font, fill=(20, 11, 15))
+
+    img_draw.text((360, 630), pln, font=date_font, fill=(134, 31, 45))
+    img_draw.text((620, 630), str(curs_nb_one(max_date_curs_nb())['pln']), font=kurs_font, fill=(134, 31, 45))
+    img_draw.text((360, 690), line, font=line_font, fill=(20, 11, 15))
+
+    # img.show()
+
     return img
